c63d24dca3e0a88b584522781be14feac08cdb63
uzairahmednasir <uzairahmednasir@outlook.com>
2022-07-14 00:40:40 +0500

    first commit

---
+++ src/chatapplicationjava/ChatApplicationJava.java
@@ -0,0 +1,12 @@
+package chatapplicationjava;
+
+import Components.Registration.Login;
+
+public class ChatApplicationJava {
+    
+    public static void main(String[] args) {
+        Login loginPanel = new Login();
+        loginPanel.setVisible(true);
+    }
+    
+}
